30c9a918db7df9722c0589e6cbe8356b293c5b84
MuhammadZakwanSakhiy <muhammadzakwansakhiy@gmail.com>
2026-03-29 14:44:21 +0700

    Membuat website react 0.0 yang sebelumnya dibuat berdasarkan UI figma dengan tailwind saja

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,8 +8,9 @@
         <link rel="icon" href="/favicon.svg" type="image/svg+xml">
         <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com" rel="stylesheet">
+        <link href="https://fonts.cdnfonts.com/css/helvetica-255" rel="stylesheet">
+
 
         @viteReactRefresh
         @vite(['resources/css/app.css', 'resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
@@ -17,7 +18,7 @@
             <title>{{ config('app.name', 'Laravel') }}</title>
         </x-inertia::head>
     </head>
-    <body class="font-sans antialiased">
+    <body class="min-h-screen bg-linear-to-b from-[#324879] to-[#1E2E50]">
         <x-inertia::app />
     </body>
 </html>
